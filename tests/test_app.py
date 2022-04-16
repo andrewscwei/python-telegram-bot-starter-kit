50f4e307b41baa7fbbed301b098ecf02c0d1c989
Andrew Wei <andrewscwei@gmail.com>
2022-04-16 11:52:25 -0700

    Fix tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,5 @@
 import os
 
-from app.bot import dispatcher
-
 
 def test_bot_token():
   assert 'BOT_TOKEN' in os.environ
-
-def test_bot_dispatcher():
-  assert dispatcher is not None
-  assert dispatcher.bot is not None
-  assert dispatcher.bot.token is not None
